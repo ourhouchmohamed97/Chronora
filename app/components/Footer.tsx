af151b753adc1759744ad5597916a41f4607946d
haytham <ezzahirhaytham@gmail.com>
2026-02-24 16:39:10 +0000

    add forgot_password page

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -63,7 +63,7 @@ const Footer: React.FC = () => {
         {/* Bottom Bar */}
         <div className="border-t border-gray-200 mt-8 pt-8 flex flex-col md:flex-row justify-between items-center">
           <p className="text-gray-600 text-sm">
-            © {currentYear} Chirona. All rights reserved.
+            © {currentYear} Chronora. All rights reserved.
           </p>
           <div className="flex space-x-6 mt-4 md:mt-0">
             <a href="#" className="text-sm text-gray-600 hover:text-indigo-600">Privacy Policy</a>
